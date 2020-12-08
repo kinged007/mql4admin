fdf26ddddb8001772d8d75ca510cdd0c9209606f
kinged007 <joshuab.za@gmail.com>
2020-12-08 16:14:33 +0100

    fixes 2

--- mt4status.php
+++ mt4status.php
@@ -248,10 +248,11 @@ include BASE_PATH . '/includes/header.php';
                             ?>
                         <td<?= $style; ?>>
                             <?php 
-                                if(!empty($style)) 
+                                if(!empty($style)) {
                                     echo "<span class='badge badge-danger'>Offline for ";
                                     echo (time()-strtotime($row['ping'])>24*60*60) ? floor(abs(time() - strtotime($row['ping'])) / 86400) . " days, " . date("H:i",time()-strtotime($row['ping'])) : date("H:i",time()-strtotime($row['ping']));
                                     echo " (Hr:min)</span><br/>"; 
+                                }
                             ?>
 
                             Server: <?php echo htmlspecialchars($row['ping']); ?><br/>
